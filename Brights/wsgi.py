551f975a62c7f77e0469752cf37567d20aa0e22d
Dipu Howlader <dipuhowlader33@gmail.com>
2021-09-02 02:14:39 +0600

    updated

--- Brights/wsgi.py
+++ Brights/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for brights project.
+WSGI config for Brights project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brights.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Brights.settings')
 
 application = get_wsgi_application()
--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn brights.wsgi
+web: gunicorn Brights.wsgi
