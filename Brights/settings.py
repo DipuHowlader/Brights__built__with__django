7a248c4dad5c51f5d7b7b461af652d86d89a23f6
Dipu Howlader <dipuhowlader33@gmail.com>
2021-09-02 01:19:49 +0600

    updated

--- Brights/settings.py
+++ Brights/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -157,5 +158,6 @@ CSRF_COOKIE_SECURE = True
 
 
 # whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
